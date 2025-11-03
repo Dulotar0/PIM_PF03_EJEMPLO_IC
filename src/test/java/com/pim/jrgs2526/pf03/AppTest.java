abce6f771125b53c8426a0c76a36dde4f7561ec0
Eric Albert Alonso <151619697+Dulotar0@users.noreply.github.com>
2025-11-03 18:27:36 +0100

    Fix parameter name in ValueSource annotations

--- src/test/java/com/pim/jrgs2526/pf03/AppTest.java
+++ src/test/java/com/pim/jrgs2526/pf03/AppTest.java
@@ -3,13 +3,15 @@ package com.pim.jrgs2526.pf03;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
     @ParameterizedTest
-    @ValueSource(int = { 5,6,7,8,9,10 })
+    @ValueSource(ints = { 5,6,7,8,9,10 })
     public void testCalcularAprobado(int nota) {
         App main = new App();
         int resultado = main.calcularNota(nota);
@@ -17,7 +19,7 @@ public class AppTest {
     }
 
     @ParameterizedTest
-    @ValueSource(int = { 0,1,2,3,4 })
+    @ValueSource(ints = { 0,1,2,3,4 })
     public void testCalcularSuspenso(int nota) {
         App main = new App();
         int resultado = main.calcularNota(nota);
